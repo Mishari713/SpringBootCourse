ec807bafb3c03667805d42d4ad9965278f1d92a7
Mishari <mishari@iastate.edu>
2025-08-14 00:53:20 +0300

    First Commit

---
+++ myapp/myapp/src/main/java/com/springboot/demo/myapp/rest/RestController.java
@@ -0,0 +1,13 @@
+package com.springboot.demo.myapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+    // expose "/" that return "Hello World!"
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World!";
+    }
+}
